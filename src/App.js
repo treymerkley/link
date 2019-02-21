64fe6c89f5d7ee8633802550664d04eca00fcdf1
Billy Wade <billy.wade@protonmail.ch>
2019-02-21 14:48:22 -0600

    rebuilt using create-react-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <form>
+	    <input placeholder="enter task">
+	    </input>
+	    <button type="submit">add</button>
+	    </form>
+	    </header>
+	    </div>
+    );
+  }
+}
+
+export default App;
