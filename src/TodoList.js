c3c614c7151b379683056b9bb886558176d322eb
Billy Wade <billy.wade@protonmail.ch>
2019-03-08 16:02:09 -0600

    completed initial UI

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
         };
         
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
     addItem(e) {
         if (this._inputElement.value !== "") {
@@ -32,6 +33,16 @@ class TodoList extends Component {
 
         e.preventDefault();
     }
+
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+
+        this.setState ({
+            items: filteredItems
+        });
+    }
     
     render() {
         return (
@@ -44,7 +55,8 @@ class TodoList extends Component {
                   <button type="submit">add</button>
                 </form>
               </div>
-              <TodoItems entries={this.state.items}/>
+              <TodoItems entries={this.state.items}
+            delete={this.deleteItem}/>
             </div>
         );
     }
